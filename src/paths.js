40f6b12d84735cb9d8e6211bf8366fcaf6467ed4
Henrik Hofmeister <henrik@newdawn.dk>
2023-01-07 10:37:33 +0100

    Use auth file path from cluster config

--- src/paths.js
+++ src/paths.js
@@ -1,12 +1,12 @@
 const ClusterConfiguration = require("@blockware/local-cluster-config");
 const Path = require("path");
 const BASEDIR_BLOCKWARE = ClusterConfiguration.getBlockwareBasedir();
+const AUTH_TOKEN = ClusterConfiguration.getAuthenticationPath();
+
 const BASEDIR_USER = Path.join(BASEDIR_BLOCKWARE, 'blockctl');
-
 const USER_COMMANDS = BASEDIR_USER + '/commands.json';
-const AUTH_TOKEN = BASEDIR_USER + '/authentication.json';
-
 const BASEDIR_COMMANDS = Path.join(__dirname, '../commands');
+
 const DEFAULT_COMMANDS = Path.normalize(__dirname + '/../default-commands.json');
 
 const ALL_COMMANDS = [
