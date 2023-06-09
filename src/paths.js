688b91a017e9430de0200517ca467fc5eea84672
Jacob Wejendorp <jacob@wejendorp.dk>
2023-06-09 16:08:40 +0200

    fix: patch cluster-config import

--- src/paths.js
+++ src/paths.js
@@ -2,24 +2,28 @@ const ClusterConfiguration = require("@kapeta/local-cluster-config").default;
 const Path = require("path");
 const BASEDIR_KAPETA = ClusterConfiguration.getKapetaBasedir();
 
-const BASEDIR_USER = Path.join(BASEDIR_KAPETA, 'cli');
-const USER_COMMANDS = Path.join(BASEDIR_USER , 'commands.json');
-const BASEDIR_COMMANDS = Path.join(BASEDIR_USER, 'commands');
+const BASEDIR_USER = Path.join(BASEDIR_KAPETA, "cli");
+const USER_COMMANDS = Path.join(BASEDIR_USER, "commands.json");
+const BASEDIR_COMMANDS = Path.join(BASEDIR_USER, "commands");
 
-const DEFAULT_COMMANDS = Path.normalize(Path.join(__dirname , '/../default-commands.json'));
-const CI_COMMANDS = Path.normalize(Path.join(__dirname , '/../ci-commands.json'));
+const DEFAULT_COMMANDS = Path.normalize(
+  Path.join(__dirname, "/../default-commands.json")
+);
+const CI_COMMANDS = Path.normalize(
+  Path.join(__dirname, "/../ci-commands.json")
+);
 
 const ALL_COMMANDS = [
-    USER_COMMANDS,
-    process.env.KAPETA_CI ? CI_COMMANDS : DEFAULT_COMMANDS
+  USER_COMMANDS,
+  process.env.KAPETA_CI ? CI_COMMANDS : DEFAULT_COMMANDS,
 ];
 
 module.exports = {
-    BASEDIR_KAPETA,
-    BASEDIR_USER,
-    USER_COMMANDS,
-    BASEDIR_COMMANDS,
-    DEFAULT_COMMANDS,
-    CI_COMMANDS,
-    ALL_COMMANDS
+  BASEDIR_KAPETA,
+  BASEDIR_USER,
+  USER_COMMANDS,
+  BASEDIR_COMMANDS,
+  DEFAULT_COMMANDS,
+  CI_COMMANDS,
+  ALL_COMMANDS,
 };
